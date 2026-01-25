552e1728baefbf2e773c5a765c31cfecd64cf257
Kerimli <qeribkerimli99@gmail.com>
2026-01-25 21:10:44 +0400

    chore(): add LumenMobileAppApplicationTests.java

---
+++ src/test/java/logtracker/pocket/lumenmobileapp/LumenMobileAppApplicationTests.java
@@ -0,0 +1,13 @@
+package logtracker.pocket.lumenmobileapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class lumenmobileappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
